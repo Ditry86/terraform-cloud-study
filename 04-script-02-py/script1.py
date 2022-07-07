9cce605f570074a3b775ff1b5ccd138fa74cad4f
ditry86 <suetin.dmitry@gmail.com>
2022-07-08 04:14:48 +1000

    Second Commit

--- 04-script-02-py/script1.py
+++ 04-script-02-py/script1.py
@@ -3,18 +3,17 @@
 import os
 
 
-path="/home/ditry/study/devops-netology"
-bash_command = ["cd "+path, "git status -u"]
+path="/home/ditry/study/devops-netology/"
+bash_command = ["cd "+path, "git status --ignored=no -s"]
 result_os = os.popen(' && '.join(bash_command)).read()
 is_change = 0
+changes=['AM','AD','MD','MM','A','M','D','??']
 for result in result_os.split('\n'):
-    if result.find('изменено') != -1:
-        is_change='изменено'
-    elif result.find('новый файл') != -1:
-        is_change='новый файл'
-    elif result.find('удалено') != -1:
-        is_change='удалено'
+    for x in changes :
+        if result.find(x) != -1:
+            is_change = x
+            break
     if is_change != 0:
-        prepare_result = result.replace('\t'+is_change+':   ', '\t'+is_change+'/home/ditry/study/devops-netology')
-        print(result)
+        prepare_result = result.replace(is_change+' ', is_change+' '+path)
+        print(prepare_result)
         is_change=False
