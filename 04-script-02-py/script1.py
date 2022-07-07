a0283d55d23f2a10ed26373a01e54841be63bca9
ditry86 <suetin.dmitry@gmail.com>
2022-07-08 03:18:15 +1000

    Second commit

---
+++ 04-script-02-py/script1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+
+path="/home/ditry/study/devops-netology"
+bash_command = ["cd "+path, "git status -u"]
+result_os = os.popen(' && '.join(bash_command)).read()
+is_change = 0
+for result in result_os.split('\n'):
+    if result.find('изменено') != -1:
+        is_change='изменено'
+    elif result.find('новый файл') != -1:
+        is_change='новый файл'
+    elif result.find('удалено') != -1:
+        is_change='удалено'
+    if is_change != 0:
+        prepare_result = result.replace('\t'+is_change+':   ', '\t'+is_change+'/home/ditry/study/devops-netology')
+        print(result)
+        is_change=False
+    #else :
+     #   break
