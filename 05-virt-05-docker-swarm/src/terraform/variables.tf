387899b5746c807ae240a43b092e4a83a698da65
ditry86 <suetin.dmitry@gmail.com>
2022-08-01 05:16:01 +1000

    05-virt-05-docker-swarm

---
+++ 05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gpu94shmdph5555lnu"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g7r7ds9ra1dmhiri72"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8sikqm3g6lm8q2hj94"
+}
