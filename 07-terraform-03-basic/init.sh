ef6a5c8a1e80ba171f5a34514fc1becf14b31d1d
ditry86 <suetin.dmitry@gmail.com>
2022-10-27 19:11:03 +0900

    Modify init.sh

--- 07-terraform-03-basic/init.sh
+++ 07-terraform-03-basic/init.sh
@@ -1,76 +1,31 @@
 #!/usr/bin/env bash
 echo $'\n'Get vars
 oa_token=$(cat init.conf | grep oa_token | sed 's/oa_token = //')
-account_id=$(cat init.conf | grep sevice_acc_id | sed 's/sevice_acc_id = //')
 cloud_id=$(cat init.conf | grep cloud_id | sed 's/cloud_id = //')
 folder_id=$(cat init.conf | grep folder_id | sed 's/folder_id = //')
 account=$(cat init.conf | grep account | sed 's/account = //')
 zone=$(cat init.conf | grep zone | sed 's/zone = //')
 storage_account=$(cat init.conf | grep storage_account | sed 's/storage_account = //')
+backet=$(cat init.conf | grep backet | sed 's/backet = //')
 
 echo $'\n'Set yc
-yc config set token $oa_token
-yc iam key create --service-account-id $account_id --output key.json 
-yc config profile create $account
+yc config set token ${oa_token}
+serv_acc_id=$(yc iam service-account create --name ${account} --folder-name=default | grep ^id: | sed 's/id: //')
+yc iam key create --service-account-id ${serv_acc_id} --output key.json 
+yc config profile create ${account}
 yc config set service-account-key key.json
-yc config set cloud-id $cloud_id
-yc config set folder-id $folder_id
+yc config set cloud-id ${cloud_id}
+yc config set folder-id ${folder_id}
 
 echo $'\n'Set env vars
 export YC_TOKEN=$(yc iam create-token)
-export YC_CLOUD_ID=$(yc config get cloud-id)
-export YC_FOLDER_ID=$(yc config get folder-id)
+export YC_CLOUD_ID=${cloud_id}
+export YC_FOLDER_ID=${folder_id}
 
-echo $'\n'Set terra provider
-cat > ~/.terraformrc << _EOF_
-provider_installation {
-  network_mirror {
-    url = "https://terraform-mirror.yandexcloud.net/"
-    include = ["registry.terraform.io/*/*"]
-  }
-  direct {
-    exclude = ["registry.terraform.io/*/*"]
-  }
-}
-_EOF_
+export TF_VAR_stor_serv_acc_id=$(yc iam service-account create --name ${storage_account} --folder-name=default | grep ^id: | sed 's/id: //')
+yc resource-manager folder add-access-binding default --role storage.admin --subject serviceAccount:${TF_VAR_stor_serv_acc_id}
+yc iam access-key create --service-account-name ${storage_account} >> access.key
+export TF_VAR_access_key=$(cat access.key | grep key_id: | sed 's/key_id: //')
+export TF_VAR_secret_key=$(cat access.key | grep secret: | sed 's/secret: //')
 
-echo $'\n'Init main.tf
-cat > ./main.tf << _EOF_
-provider "yandex" {
-  zone = "$zone"
-}
-data "yandex_compute_image" "ubuntu_22" {
-  family = "ubuntu-2204-lts"
-}
-_EOF_
 
-echo $'\n'Init version.tf
-cat > ./versions.tf << _EOF_
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
- required_version = ">= 0.13"
-}
-_EOF_
-
-echo $'\n'Terraform init
-terraform init
-
-echo $'\n'Init ssh keys
-ssh-keygen -t ed25519 -N $account -f $account'_key'
-ssh_key=$(cat  $account'_key.pub')
-
-echo $'\n'Create VM user params
-cat > ./inst_meta.conf << _EOF_
-#cloud-config
-users:
-  - name: $account
-    groups: sudo
-    shell: /bin/bash
-    sudo: ['ALL=(ALL) NOPASSWD:ALL']
-    ssh_authorized_keys:
-      - $ssh_key
-_EOF_
