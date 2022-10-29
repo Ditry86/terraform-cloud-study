5dbd13c3198ae58e3486ca6458ae6eeab49b3ce4
Ditry86 <52617296+Ditry86@users.noreply.github.com>
2022-10-30 01:03:53 +1000

    Delete 07-terraform-03-basic directory

--- 07-terraform-03-basic/prepare_storage.sh
+++
@@ -1,3 +0,0 @@
-export TF_VAR_stor_serv_acc_id=$(yc iam service-account create --name storage-admin| grep ^id: | sed 's/id: //')
-yc resource-manager folder add-access-binding default --role storage.admin --subject serviceAccount:${STOR_SERV_ACC_NAME}
-yc iam access-key create --service-account-name ${STOR_SERV_ACC_NAME}
