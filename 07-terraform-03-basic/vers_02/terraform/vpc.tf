d030167f4c53cc11f61704a6e7a2e83173c0ca0f
ditry86 <suetin.dmitry@gmail.com>
2022-11-02 17:14:42 +0900

    07-terraform-03-basic

--- 07-terraform-03-basic/vers_02/terraform/vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "yandex_vpc_network" "netology_net" {
-}
-
-resource "yandex_vpc_subnet" "terraform_07_03_subnet" {
-  name           = "${terraform.workspace}-lan"
-  network_id     = yandex_vpc_network.netology_net.id
-  v4_cidr_blocks = "${local.workspace_lans[terraform.workspace]}"
-}
