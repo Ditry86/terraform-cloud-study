dadd89993289833dcd12ddbdcbc2a2734267ee89
ditry86 <suetin.dmitry@gmail.com>
2022-11-06 16:29:01 +0900

    reworked

--- reinit_env.sh
+++
@@ -1,4 +0,0 @@
-export YC_TOKEN=$(yc iam create-token)
-export YC_CLOUD_ID=$(yc config get cloud-id)
-export YC_FOLDER_ID=$(yc config get folder-id)
-export YC_ZONE=$(cat init.conf | grep zone | sed 's/zone = //')
